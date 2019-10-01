7c6736b9a593e56b34a106aeb4150e029222f104
skupavsk <skupavsk@c02yh0wwjgh6.uk.aexp.com>
2019-10-01 15:19:42 +0100

    Can view all books in alphab. order with JDBC

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -2,18 +2,17 @@ package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String database = "library";
+        String user = "root";
+        String password = "passwordsql";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -21,14 +20,39 @@ public class Main {
     }
 
     private static void jdbcMethod(String connectionString) throws SQLException {
-        System.out.println("JDBC method...");
+
 
         // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
         Connection connection = DriverManager.getConnection(connectionString);
 
+        String database = "library";
 
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+
+            String query = "select title " +
+                    "from " + database + ".books ORDER BY title;";
+
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                String title = rs.getString("title");
+                System.out.println(title);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+        }
 
     }
 
